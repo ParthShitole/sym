11e9de4adccefe855eff351a752957972d45b001
ParthShitole <101716280+ParthShitole@users.noreply.github.com>
2023-05-26 00:27:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-var videos = ["video1", "video2", "video3"];
-var currentVideoIndex = 0;
 
-function playNextVideo() {
-  var currentVideo = document.getElementById(videos[currentVideoIndex]);
-  currentVideo.style.display = "none";
-  currentVideoIndex = (currentVideoIndex + 1) % videos.length;
-  var nextVideo = document.getElementById(videos[currentVideoIndex]);
-  nextVideo.style.display = "block";
-  nextVideo.play();
-  currentVideo.pause();
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-var nextButton = document.getElementById("next-button");
-nextButton.addEventListener("click", playNextVideo);
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
 
 
 
